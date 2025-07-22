da72ee7bfbc4df760992f2c0a7d111710f195295
Arpan-Patel-07 <abdhankecha802@gmail.com>
2025-07-22 23:32:17 +0530

    Add files via upload

---
+++ Task-1.py
@@ -0,0 +1,19 @@
+#Program to take two numbers from user and perform basic mathematical operation.
+
+
+a = input("Enter the first number : ")      
+b = input("Enter the second number : ")
+
+a=int(a)
+b=int(b)
+
+s1 = a+b        #solution with addition
+s2 = a-b        #solution with subtraction        
+s3 = a*b        #solution with multiplication
+s4 = a/b        #solution with Division
+
+print("Addition is ", s1)
+print("Subtraction is ", s2)
+print("multiplication is ", s3)
+print("Division is ", s4)
+
