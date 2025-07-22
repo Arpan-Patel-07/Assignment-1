50bea9df55b2626b2595968dcb7b66bebf57e79e
Arpan-Patel-07 <abdhankecha802@gmail.com>
2025-07-22 23:32:44 +0530

    Add files via upload

---
+++ Task-2.py
@@ -0,0 +1,7 @@
+#Take users first and last name and print a greeting message with user's full name
+
+first_name = input("Enter your first name : ")
+last_name = input("Enter your second name : ")
+
+full_name = first_name + " " + last_name
+print("Hello, " + full_name + "! \nGood Morning.")
